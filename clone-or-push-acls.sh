5098b446fbe570e8ddbd800d07df8f4e9366d400
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-02-24 11:55:54 +0000

    Fix syntax.

--- clone-or-push-acls.sh
+++ clone-or-push-acls.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Change to working directory:
 cd ${STORAGE_PATH_SHARED}
 
-$FOLDER=wayback_excludes_update
-$URL = https://git.wa.bl.uk/bl-services/wayback_excludes_update.git
+# Set up variables:
+FOLDER=wayback_excludes_update
+URL=https://git.wa.bl.uk/bl-services/wayback_excludes_update.git
 
+# Clone or pull as appropriate:
 if [ ! -d "$FOLDER" ] ; then
     git clone $URL $FOLDER
 else
