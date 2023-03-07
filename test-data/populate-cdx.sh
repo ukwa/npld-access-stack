8a05e0bb529143a5afc1c5e628f1b1adf87a5fe0
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-03-07 13:47:58 +0000

    Fix quotes.

--- test-data/populate-cdx.sh
+++ test-data/populate-cdx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "Sleeping for 15 seconds while the OutbackCDX server starts up...
+echo "Sleeping for 15 seconds while the OutbackCDX server starts up..."
 sleep 15
 
-echo "Now posting the data to the CDX server...
+echo "Now posting the data to the CDX server..."
 curl -X POST --data-binary @/test-data/portico.bl.uk-data-102148-31031342-BL-31031342-0.cdx http://localhost:8080/tc
 
