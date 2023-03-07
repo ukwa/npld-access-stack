c065653543b2ab8d2e14862ffb90bd78bbdc9b79
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-03-07 13:30:37 +0000

    Fix the port.

--- test-data/populate-cdx.sh
+++ test-data/populate-cdx.sh
@@ -4,5 +4,5 @@ echo "Sleeping for 15 seconds while the OutbackCDX server starts up...
 sleep 15
 
 echo "Now posting the data to the CDX server...
-curl -X POST --data-binary @/test-data/portico.bl.uk-data-102148-31031342-BL-31031342-0.cdx http://localhost:19090/tc
+curl -X POST --data-binary @/test-data/portico.bl.uk-data-102148-31031342-BL-31031342-0.cdx http://localhost:8080/tc
 
